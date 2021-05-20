d02fec85eb563e1ad90fe9525e27966b6ff416e0
João Pinheiro <joao.castropinheiro@gmail.com>
2021-05-20 14:42:15 +0100

    master

--- .etc/setup.sh
+++ .etc/setup.sh
@@ -4,11 +4,11 @@ if ! [ "$(id -u)" = 0 ]; then
   echo "run as root"
   exit 1
 fi
-fd -t f -E setup.sh -E srsbsns.conf -E swayshell.desktop -E pkglist.txt -x sh -c '
+fd -t f -E setup.sh -E srsbsns.conf -E srsbsns-lts.conf -E swayshell.desktop -E pkglist.txt -x sh -c '
   echo linking {}
   mkdir -p /etc/{//}
   rm /etc/{}
   ln {} /etc/{}'
-cp srsbsns.conf /boot/loader/entries/
+cp srsbsns.conf srsbsns-lts.conf /boot/loader/entries/
 cp swayshell.desktop /usr/share/wayland-sessions/
 dircolors ~/.dircolors > ~/.ls_colors; chown pineman: ~/.ls_colors
