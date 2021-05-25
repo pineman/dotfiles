827d6874a646fe38b77058e37c89a924f99623e5
João Pinheiro <joao.castropinheiro@gmail.com>
2021-05-25 12:59:16 +0100

    concurrent tasks

--- .local/bin/backup.sh
+++ .local/bin/backup.sh
@@ -5,6 +5,7 @@ zpool import -o cachefile=none -o altroot=/hdd -N hdd || true
 last_recv=$(zfs list -H -o name -t snapshot -s creation hdd | tail -n 1 | cut -d'@' -f2)
 new=$(perl -l -e 'print ++$ARGV[0]' $last_recv)
 zfs snapshot -r ssd@$new
-zfs send -R -I $last_recv ssd@$new | zfs recv -Fdu hdd
-backblaze-b2 sync --noProgress --keepDays 30 --threads 8 /home/pineman/Documents/.zfs/snapshot/$new b2://pineman-Documents
+zfs send -R -I $last_recv ssd@$new | zfs recv -Fdu hdd &
+backblaze-b2 sync --noProgress --keepDays 30 --threads 8 /home/pineman/Documents/.zfs/snapshot/$new b2://pineman-Documents &
+wait
 zpool export hdd
