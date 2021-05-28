5b489fdbe4c9be085a1902437a85e31f0764920a
João Pinheiro <joao.castropinheiro@gmail.com>
2021-05-28 10:27:13 +0100

    cenas

--- .root/setup.sh
+++ .root/setup.sh
@@ -5,11 +5,11 @@ if ! [ "$(id -u)" = 0 ]; then
   exit 1
 fi
 dircolors ~/.dircolors > ~/.ls_colors; chown pineman: ~/.ls_colors
-fd -I -H -t f \
--x sh -c '
+(cd etc && fd -I -H -t f -x sh -c '
   echo syncing {}
   mkdir -p /etc/{//}
-  unison -batch -silent {} /etc/{}' . etc
+  unison -batch -silent {} /etc/{}')
 unison -batch -silent -fat boot/loader /boot/loader
+cp us-esc-caps.map.gz /usr/share/kbd/keymaps/i386/qwerty/
 yay -Qqs | tail -n +2 > $(cat /etc/hostname)-pkglist.txt
 chown -R pineman: .
