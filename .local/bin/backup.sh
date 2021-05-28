8d81ddc634ab3ae5faa60b5bde7f39f70924674a
João Pinheiro <joao.castropinheiro@gmail.com>
2021-05-28 10:31:56 +0100

    cenas

--- .local/bin/backup.sh
+++ .local/bin/backup.sh
@@ -6,7 +6,6 @@ last_recv=$(zfs list -H -o name -t snapshot -s creation hdd | tail -n 1 | cut -d
 #new=$(perl -l -e 'print ++$ARGV[0]' $last_recv)
 new=$(ruby -e 'puts ARGV[0].succ' $last_recv)
 zfs snapshot -r ssd@$new
-zfs send -R -I $last_recv ssd@$new | zfs recv -Fdu hdd &
-backblaze-b2 sync --noProgress --keepDays 30 --threads 8 /home/pineman/Documents/.zfs/snapshot/$new b2://pineman-Documents &
-wait
+zfs send -R -I $last_recv ssd@$new | zfs recv -Fdu hdd
+backblaze-b2 sync --noProgress --keepDays 30 --threads 8 --replaceNewer /home/pineman/Documents/.zfs/snapshot/$new b2://pineman-Documents
 zpool export hdd
