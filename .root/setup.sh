704f916c4b1ab5e6e2613702e1b43b3c55cdcb2a
João Pinheiro <joao.castropinheiro@gmail.com>
2021-12-07 08:27:59 +0000

    more cenas

--- .root/setup.sh
+++ .root/setup.sh
@@ -8,9 +8,16 @@ fd -I -H -t f . etc -x sh -c '
   echo overwriting /{}
   mkdir -p /{//}
   cp -f {} /{}'
+rm -rf /boot/loader/loader.conf /boot/loader/entries/*
 cp -rf boot/loader/loader.conf boot/loader/entries /boot/loader
 cp us-esc-caps.map.gz /usr/share/kbd/keymaps/i386/qwerty/
 yay -Qqs | tail -n +2 > $(cat /etc/hostname)-pkglist.txt
 chown -R pineman: .
 dircolors ~pineman/.dircolors > ~pineman/.ls_colors
 chown pineman: ~pineman/.ls_colors
+#locale-gen
+#timedatectl set-ntp true
+#timedatectl set-timezone Europe/Lisbon
+#systemctl enable systemd-resolved NetworkManager sshd bluetooth --now
+#mkdir /var/tmp/{ccache,aur}
+#chown -R pineman: /var/tmp/{ccache,aur}
