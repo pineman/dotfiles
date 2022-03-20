b923f19c0a2bbd181861b082538551f3a07422ee
João Pinheiro <joao.castropinheiro@gmail.com>
2022-03-20 04:03:34 +0000

    more packages

--- .root/packages.sh
+++ .root/packages.sh
@@ -1,40 +1,64 @@
 #!/bin/bash
 set -euxo pipefail
 packages=(
+  # System
+  arch-install-scripts
+  pacman-contrib
+  kernel-modules-hook
   man-db
+  # Utilities
   zsh
   neovim
   nvimpager-git
   ripgrep
   sd
   htop-vim
+  dfc
+  ncdu
+  git-delta
+  terminus-font
+  tlp
+  mlocate
+  backblaze-b2-bin
+  rclone
+  direnv
+  fasd
+  physlock
+  # Graphical
   sway
   xorg-xwayland
   mako
   i3status
+  ttf-font-icons
+  ttf-droid
+  pipewire-jack
+  pipewire-media-session
+  polkit-gnome
   chromium
+  firefox
   alacritty
+  foot
   thunar
   gnome-keyring
   blueman
   gammastep
   swayidle
-  visual-studio-code-bin
-  docker
-  git-delta
-  terminus-font
-  ttf-font-icons
-  arch-install-scripts
-  pacman-contrib
-  ttf-droid
-  pipewire-jack
-  pipewire-media-session
-  tlp
-  kernel-modules-hook
-  mlocate
   ttc-iosevka
   wl-clipboard
   keepassxc
+  brightnessctl
+  alsa-utils
+  qalculate-gtk
+  wlr-randr
+  bemenu-wayland
+  grimshot
+  tk
+  telegram-desktop
+  ydotool
+  network-manager-applet
+  # Dev
+  visual-studio-code-bin
+  docker
 )
 yay -S --noconfirm --needed "${packages[@]}"
 code --install-extension marcoms.oceanic-plus
@@ -42,5 +66,5 @@ code --install-extension vscodevim.vim
 code --install-extension golang.Go
 code --install-extension eamodio.gitlens
 
-yay -S google-cloud-sdk terraform kubectl helm
+yay -S --noconfirm --needed google-cloud-sdk terraform kubectl helm
 #kubectl krew install exec-as
