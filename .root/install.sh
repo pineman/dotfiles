b64b05f3bfd2dc6bb2e86248297befaf8978b6bb
João Pinheiro <joao.castropinheiro@gmail.com>
2023-02-08 23:46:38 +0000

    Consolidate to one file

--- .root/install.sh
+++ .root/install.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 set -euxo pipefail
 
-# Arch Linux install script for srsbsns
+# Arch Linux install script from live USB
 # git clone https://github.com/pineman/dotfiles and run like `./install stage1`
 
+BOOT=/dev/nvme0n1p1
+ROOT=/dev/nvme0n1p5
 stage1() {
-  # Assume $DISK is a partition of type Linux
-  DISK=/dev/nvme0n1p5
-  BOOT=/dev/nvme0n1p1
   umount -R /mnt || true
-  mkfs.ext4 $DISK
-  mount $DISK /mnt
+  mkfs.ext4 $ROOT
+  mount $ROOT /mnt
   mkdir -p /mnt/boot
   mount $BOOT /mnt/boot
   echo 'Server = https://ftp.rnl.tecnico.ulisboa.pt/pub/archlinux/$repo/os/$arch' > /etc/pacman.d/mirrorlist
@@ -55,20 +54,192 @@ stage3() {
   git clone https://aur.archlinux.org/yay-bin --depth 1
   cd yay-bin
   makepkg -si --noconfirm
-  cd ~/.root
-  ./system-files.sh
+  system-files
 }
 
 stage4() {
+  # Run on newly booted system
   sudo rm -f /etc/resolv.conf
   sudo ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
   sudo timedatectl set-ntp true
   sudo timedatectl set-timezone Europe/Lisbon
-  ./packages.sh
-  echo 'copy keepass'
-  echo 'setup ssh key'
+  packages
   git remote remove origin
   git remote add origin git@github.com:pineman/dotfiles.git
+  echo 'copy keepass, setup ssh key'
+}
+
+system-files() {
+  fd -I -H -t f . etc -x sh -c '
+    echo overwriting /{}
+    sudo mkdir -p /{//}
+    sudo cp -f {} /{}'
+  sudo rm -rf /boot/loader/loader.conf /boot/loader/entries/*
+  sudo cp -rf boot/loader/loader.conf boot/loader/entries /boot/loader
+  offset=$(sudo filefrag -v /swapfile | awk '$1=="0:" {print substr($4, 1, length($4)-2)}')
+  sudo sed -i "s%#ROOT#%$ROOT%g" /boot/loader/entries/*
+  sudo sed -i "s%#OFFSET#%$offset%g" /boot/loader/entries/*
+  sudo cp us-esc-caps.map.gz /usr/share/kbd/keymaps/i386/qwerty/
+  dircolors ~/.dircolors > ~/.ls_colors
+}
+
+packages() {
+  packages=(
+    # System
+    arch-install-scripts
+    pacman-contrib
+    pkgfile
+    fakepkg
+    kernel-modules-hook
+    man-db
+    man-pages
+    nano
+    systemd-resolvconf
+    wireless-regdb
+    # Utilities
+    zsh
+    zsh-completions
+    bash-completion
+    neovim
+    nvimpager-git
+    ripgrep
+    sd
+    htop-vim
+    dfc
+    ncdu
+    git-delta
+    backblaze-b2-bin
+    rclone
+    direnv
+    fasd
+    physlock
+    parallel
+    openbsd-netcat
+    strace
+    #bcc
+    #bcc-tools
+    #python-bcc
+    acpi
+    nmap
+    traceroute
+    ddcci-driver-linux-dkms
+    ddcutil
+    bluez
+    bluez-utils
+    trash-cli
+    imagemagick
+    plocate
+    moreutils
+    tree
+    ifuse
+    usbutils
+    sysstat
+    radeontop
+    # Graphical
+    sway
+    xorg-xwayland
+    mako
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-extra
+    ttf-apple-emoji
+    ttf-dejavu
+    pipewire-pulse
+    pipewire-jack
+    pipewire-alsa
+    wireplumber
+    xdg-desktop-portal-wlr
+    pamixer
+    pavucontrol
+    polkit-gnome
+    chromium
+    firefox
+    alacritty
+    foot
+    thunar
+    gvfs
+    gvfs-mtp
+    gvfs-gphoto2
+    ntfs-3g
+    exfatprogs
+    gparted
+    tumbler
+    ffmpegthumbnailer
+    thunar-volman
+    thunar-archive-plugin
+    file-roller
+    gnome-keyring
+    blueman
+    gammastep
+    #python-xdg
+    swayidle
+    #sway-mpris-idle-inhibit
+    #sway-video-idle-inhibit
+    ttc-iosevka
+    wl-clipboard
+    keepassxc
+    qt5-wayland
+    brightnessctl
+    alsa-utils
+    qalculate-gtk
+    wlr-randr
+    bemenu-wayland
+    grimshot
+    tk
+    telegram-desktop
+    network-manager-applet
+    wev
+    mpv
+    mpv-mpris
+    vlc
+    yt-dlp
+    ristretto
+    playerctl
+    wf-recorder
+    wtype
+    zathura
+    zathura-pdf-poppler
+    waybar
+    otf-font-awesome
+    wdisplays
+    gnome-font-viewer
+    gucharmap
+    dconf-editor
+    pinta
+    gimp
+    inkscape
+    rmlint
+    rmlint-shredder
+    transmission-gtk
+    dragon-drop
+    libva-mesa-driver
+    libva-utils
+    # Dev
+    jq
+    gron
+    wireshark-qt
+    visual-studio-code-bin
+    #docker
+    #docker-compose
+    pigz
+    shellcheck-bin
+    flyctl-bin
+    rlwrap
+    python-pip
+    #rbenv
+    #ruby-build
+  )
+  yay -S --noconfirm --needed "${packages[@]}"
+  echo 'pineman' | sudo chsh -s /bin/zsh pineman
+  sudo systemctl enable linux-modules-cleanup fstrim.timer bluetooth plocate-updatedb.timer
+  #sudo gpasswd -a pineman docker
+  sudo gpasswd -a pineman wireshark
+  sudo gpasswd -a pineman seat
+  gsettings set 'org.gnome.desktop.interface' font-name 'sans 10'
+  mkdir ~/.local/log
+  pip install dtrx
+  code --install-extension marcoms.oceanic-plus
+  code --install-extension asvetliakov.vscode-neovim
 }
 
 "$@"
